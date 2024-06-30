13b56152823dfb6c08e94ee1d72675f9fcb14cb0
zhendler <zhendlerbing@gmail.com>
2024-06-30 17:36:02 +0300

    17.35

--- modul 3/modul 3 task 2.py
+++ modul 3/modul 3 task 2.py
@@ -2,15 +2,16 @@ import random
 
 
 def get_numbers_ticket(min, max, quantity):
-    if min>=1 and max<=1000 and max>min and quantity>max-min+1:
+    if min<1 and max<1000 and max<min and quantity<max-min+1:
         print('u do it wrong')
-    winners_set=set()
-    while len(winners_set) <= quantity:
-        random_number = random.randint(min,max)  
-        winners_set.add(random_number)
+    else:
+        winners_set=set()
+        while len(winners_set) < quantity:
+            random_number = random.randint(min,max)  
+            winners_set.add(random_number)
     return print (f'The list of winning numbers is {winners_set}')
  #   else: 
  #       print('nnnnn')        
                         
 
-get_win_list = get_numbers_ticket(1,50,5) 
+get_win_list = get_numbers_ticket(1,5555,5) 
