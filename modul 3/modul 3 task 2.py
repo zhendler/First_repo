be6a35c239b418eeea12c6a5ba520d20f7f02ca7
zhendler <zhendlerbing@gmail.com>
2024-06-30 13:40:42 +0300

    13.40

---
+++ modul 3/modul 3 task 2.py
@@ -0,0 +1,16 @@
+import random
+
+
+def get_numbers_ticket(min, max, quantity):
+    if min>=1 and max<=1000 and max>min and quantity>max-min+1:
+        print('u do it wrong')
+    winners_set=set()
+    while len(winners_set) <= quantity:
+        random_number = random.randint(min,max)  
+        winners_set.add(random_number)
+    return print (f'The list of winning numbers is {winners_set}')
+ #   else: 
+ #       print('nnnnn')        
+                        
+
+get_win_list = get_numbers_ticket(1,50,5) 
