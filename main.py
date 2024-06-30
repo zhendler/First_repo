b7adafc509e3df5a564b24704b542a8bdf53dd45
zhendler <zhendlerbing@gmail.com>
2024-06-30 08:16:47 +0300

    1

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("Hello World")
-print("Universe")
+def discount_price(price, discount):
+    print(price, discount)
+    def apply_discount():
+        nonlocal price
+        price *= (1-discount)
+        print(price)
+        return price
+    apply_discount() 
+        
+        
+
+    
+    return price
+discount_price(100, 0.1)
