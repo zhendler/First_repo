3a903634e05c1144dc19673ea4ab2be87ea56dba
zhendler <zhendlerbing@gmail.com>
2024-07-07 16:58:44 +0300

    16.58

--- goit-algo-hw-04/modul 4 task 1.py
+++ goit-algo-hw-04/modul 4 task 1.py
@@ -1,27 +1,36 @@
-from pathlib import Path
+from pathlib import PurePath
 
 
 
 
 def total_salary(salary_list_file_path):
-    result = {}
-   #if salary_list_file_path.is_file():
-    with open(salary_list_file_path) as slfp:
-        for line in slfp:
-            worker_name, salary_amount = line.split(',')
-            print(worker_name), print(salary_amount)
-            #print(salary_amount)
-            salary_amount = salary_amount.rstrip('\n')
-            result[salary_amount] = worker_name
-            #print(result)
-    return result
+    try:
+        total_salary=0
+        salary_list=[]
+        with open(salary_list_file_path) as slfp:
+            for line in slfp:
+                worker_name, salary_amount = line.split(',')
+                salary_list.append(salary_amount)
+                total_salary+=float(salary_amount)
+            avarage_salary=total_salary/len(salary_list)
+            #print(total_salary)
+            #print(avarage_salary)
+            return total_salary, avarage_salary
+    except FileNotFoundError:
+        print(f"File not found: {salary_list_file_path}")
+        return (0, 0)
+    except Exception as e:
+        print(f"Error: {e}")
+        return (0, 0)
+    
 
 
 
 
 
-salary_list_file_path=Path('month_salary.txt')
-print(type(salary_list_file_path))
+salary_list_file_path=PurePath(r'C:\Users\Dell\Documents\First_repo\goit-algo-hw-04\month_salary.txt')
+
 #total_salary(salary_list_file_path)
-o = open(salary_list_file_path,'r+')
-print(o)
+total, average = total_salary(salary_list_file_path)
+
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
