8bf3a8cda2f054c3047dac8745d7bdb42a652274
zhendler <zhendlerbing@gmail.com>
2024-07-14 17:08:37 +0300

    17.08

---
+++ goit-algo-hw-05/modul 5 task 1.py
@@ -0,0 +1,36 @@
+
+def caching_fibonacci(cache_size=10):
+
+    cache = {}
+    for i in range(cache_size):
+        cache[i] = fibonacci_recursive(i)
+
+    def fibonacci(n):
+        if n <= 0:  
+            return 0
+        elif n == 1:
+            return 1
+        elif n in cache:
+            return cache[n]
+
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        return cache[n]
+    #fib_list=[num for num in cache.values()]
+    #print(fib_list)
+    return fibonacci
+
+def fibonacci_recursive(n):
+  if n <= 0:
+    return 0
+  elif n == 1:
+    return 1
+  else:
+    return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+  
+
+fib = caching_fibonacci()
+#fib_rec=fibonacci_recursive(10)
+
+#print(f' rec {fib_rec}')
+print(f'fib caching {fib(3)}')  
+print(f'fib caching {fib(6)}')  
