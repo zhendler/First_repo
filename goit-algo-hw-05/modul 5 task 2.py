c84f9c8d05a3cf617bce0bda16a82bae678affb0
zhendler <zhendlerbing@gmail.com>
2024-07-15 06:46:33 +0300

    6.46

---
+++ goit-algo-hw-05/modul 5 task 2.py
@@ -0,0 +1,18 @@
+import re
+
+def generator_numbers(text):
+    #print(text)
+    for match in re.finditer(r"\d+(\.\d+)?", text):
+        yield float(match.group())
+
+def sum_profit(text: str, func) -> float:
+
+    total_profit = 0
+    for number in func(text):
+        total_profit += number
+    return total_profit
+
+
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+total_income = sum_profit(text, generator_numbers)
+print(f"Загальний дохід: {total_income}")
--- work desk.py
+++ work desk.py
@@ -1,3 +1,18 @@
-cach = {0: 0, 1: 1, 2: 1, 3: 2, 4: 3, 5: 5, 6: 8, 7: 13, 8: 21, 9: 34}
-fib_list=[ num for num in cach.values()]
-print (fib_list)
+import re
+
+def generator_numbers(text: str) -> float:
+    #print(text)
+    for match in re.finditer(r"\d+(\.\d+)?", text):
+        yield float(match.group())
+
+def sum_profit(text: str, func) -> float:
+
+    total_profit = 0
+    for number in func(text):
+        total_profit += number
+    return total_profit
+
+
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+total_income = sum_profit(text, generator_numbers)
+print(f"Загальний дохід: {total_income}")
