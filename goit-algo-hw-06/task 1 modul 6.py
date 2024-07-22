2badd416be316012c809c8bc0d424782e7463dd6
zhendler <zhendlerbing@gmail.com>
2024-07-22 07:45:31 +0300

    7.45

--- goit-algo-hw-06/task 1 modul 6.py
+++ goit-algo-hw-06/task 1 modul 6.py
@@ -1,5 +1,5 @@
 from collections import UserDict
-
+import re
 class Field:
     def __init__(self, value):
         self.value = value
@@ -13,11 +13,16 @@ class Name(Field):
 
 class Phone(Field):
     def __init__(self, value):
-        if not value.isdigit() and len(value)!=10:
+        if not value.isdigit():
             raise ValueError("Phone number must be 10 digits")
+
+        valid_number=re.sub(r'[^\d+ ]','',value)
+        valid_number=valid_number.replace(' ','') 
+        while len(valid_number)>10:
+            valid_number=valid_number[1:]
+
+        value = ('+38' + valid_number )
         super().__init__(value)
-
-
 class Record:
     def __init__(self, name):
         self.name = Name(name)
@@ -31,33 +36,73 @@ class Record:
                  self.phones.remove(p)
 
     def edit_phone(self, old_phone, new_phone):
+        old = Phone(old_phone)
+        new = Phone(new_phone)
+
         for p in self.phones:
-            if p.value == old_phone:
+            if p.value== old.value:
                 self.phones.remove(p)
-                self.phones.append(Phone(new_phone))
+                self.phones.append(new)
+            else:
+                raise ValueError("Phone number not found")
+
+    def find_phone(self,phone):
+        f_phone=Phone(phone)
+        for p in self.phones:
+            if p == f_phone:
+                print (f"{self.name}: {f_phone.value}")
+        return None
+
 
     def __str__(self):
         return f"Contact name: {self.name}, phones: {'; '.join(p.value for p in self.phones)}"
 
 class AddressBook(UserDict):
-    def add_record(name,num):
-            pass
+    def add_record(self, record):
+
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name, None)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+
+    def __str__(self):
+        return "\n".join(str(record) for record in self.data.values())
 
 
-# Створення нової адресної книги
-#book = AddressBook()
 
-# Створення запису для John
+book = AddressBook()
+
+
 john_record = Record("John")
-
+jane_record = Record("Jane")
 john_record.add_phone("1234567890")
 john_record.add_phone("5555555555")
-print(john_record)
-john_record.edit_phone("5555555555","3332225556")
-
+jane_record.add_phone("6666666666")
+#print(john_record)
+#john_record.edit_phone("1234567890","0987654321")
+#john_record.find_phone("1234587890")
 #john_record.remove_phone("5555555555")
-print(john_record)
-# Додавання запису John до адресної книги
-##book.add_record(john_record)
+#print(john_record)
 
-# Створення та додавання нового запису для
+book.add_record(john_record)
+
+book.add_record(jane_record)
+
+
+#print(book)
+
+# Find and edit John's phone
+john = book.find("John")
+
+#print(john)  
+
+
+
+book.delete("Jane")
+
+
+#print(book)
